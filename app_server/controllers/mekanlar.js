de7cef97e85884ea70991ca03b11d0e3a1b18b74
mehmetarmagan <armagantoklu1@gmail.com>
2020-12-03 18:37:45 +0300

    ikincigonderim

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,110 +1,111 @@
 var express = require('express');
 var router = express.Router();
-const anaSayfa=function(req, res, next){
-  res.render('anaSayfa',
-  {
-    'baslik': 'Anasayfa',
-  'sayfaBaslik':{
-    'siteAd':'Mekan32',
-    'aciklama':'Isparta civarındaki mekanları keşfedin'
-  },
-  'mekanlar':[
-   {
-      'ad':'Starbucks',
-      'adres':'Centrum Garden AVM',
-      'puan':1,
-      'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
-      'mesafe':'10km'
-    },
-      {
-      'ad':'Kahve Dunyası',
-      'adres':'Centrum Garden AVM',
-      'puan':3,
-      'imkanlar':['Soğuk Kahve','Sıcak Kahve','Çay'],
-      'mesafe':'18km'
-    },
-      {
-      'ad':'Çay Ocağı',
-      'adres':'İsmetpaşa Mah',
-      'puan':5,
-      'imkanlar':['Çay','Oralet','Tost','Simit'],
-      'mesafe':'10km'
-    },
-      {
-      'ad':'Kyk Kantini',
-      'adres':'Isparta Erkek KYK ',
-      'puan':0,
-      'imkanlar':['Bozuk Peynir','Taş Ekmek','Pişmemiş Tavuk',],
-      'mesafe':'100m'
-    },
-      {
-      'ad':'Burger King',
-      'adres':'Yeşilevler Mah',
-      'puan':1,
-      'imkanlar':['Hamburger','Dondurma','İçecek'],
-      'mesafe':'19km'
-    }
-    ]
-   }
-    );
+
+const anaSayfa=function(req,res,next){
+ res.render('anaSayfa',{
+	 'baslik':'Anasayfa',
+	 'footer':'Mehmet Armağan TOKLU 2020',
+ 	 'sayfaBaslik': {
+ 		'siteAd':'Mekan32',
+ 		'aciklama':'Isparta Civarındaki Mekanları Keşfedin!!'
+ 	},
+ 	'mekanlar':[
+ 	{
+ 		'ad':'Starbucks',
+ 		'adres':'Centrum Garden AVM',
+ 		'puan':3,
+ 		'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
+ 		'mesafe':'10 km'
+ 	},
+ 	 {
+ 	 	'ad':'Kahve Dünyası',
+ 	 	'adres':'Centrum Garden AVM',
+ 	 	'puan':1,
+ 	 	'imkanlar':['Kahve','Çay','Kek'],
+ 	 	'mesafe':'17 km'
+ 	 },
+ 	 {
+ 	 	'ad':'Çay Ocağı',
+ 	 	'adres':'İsmetpaşa Mah.',
+ 	 	'puan':5,
+ 	 	'imkanlar':['Çay ','Oralet','Simit'],
+ 	 	'mesafe':'100m'
+ 	 },
+ 	 {
+ 	 	'ad':'Burger King',
+ 	 	'adres':'Yeşilevler Mah',
+ 	 	'puan':1,
+ 	 	'imkanlar':['Hamburger','Dondurma','İçecek'],
+ 	 	'mesafe':'20 km'
+ 	 },
+ 	 {
+ 	 	'ad':'Dostlar Döner Salonu',
+ 	 	'adres':'Barış Mah',
+ 	 	'puan':5,
+ 	 	'imkanlar':['Et Döner','Tavuk Döner','Ayran'],
+ 	 	'mesafe':'11 m'
+ 	 }
+ 	]
+ });
 }
 
-const mekanBilgisi=function(req, res, next){
-    res.render('mekanBilgisi',
-    {
-    'baslik': 'Mekan Bilgisi',
-    'sayfaBaslik': 'Starbucks',
-    'mekanBilgisi':{
-        'ad':'Starbucks',
-        'adres':'Centrum Garden AVM',
-        'puan':3,
-        'imkanlar':['Dünya Kahveleri','Kekleri','Pastalar'],
-        'koordinatlar':{
-          'enlem':'37.781835',
-          'boylam':'30.566034'
-        },
-        'saatler':[
-          {
-            'gunler':'Pazartesi-Cuma',
-            'acilis':'07:00',
-            'kapanis':'23:00',
-            'kapali':false
-          },
-          {
-            'gunler':'Cumartesi',
-            'acilis':'09:00',
-            'kapanis':'22:00',
-            'kapali':false
-          },
-          {
-            'gunler':'Pazar',
-            'kapali':true
-          }
-        ],
-        'yorumlar':[
-          {
-            'yorumYapan':'Mehmet Armağan TOKLU',
-            'puan':1,
-            'tarih':'02.12.2020',
-            'yorumMetni':'Buraya bir daha gitmeyin çay ocağına gidin'
-
-          },
-          {
-            'yorumYapan':'Ahmet Kaan KENAR',
-            'puan':5,
-            'tarih':'20:12:2020',
-            'yorumMetni':'Süper'
-          }
-
-        ]
-    }
-    } ) ;
+const mekanBilgisi=function(req,res,next){
+ res.render('mekanBilgisi',{
+ 	'baslik':'Mekan Bilgisi',
+	 'sayfaBaslik':'Starbucks',
+	 'footer':'Mehmet Armağan TOKLU 2020',
+ 	 'mekanBilgisi':{
+ 		'ad':'Starbucks',
+ 		'adres':'Centrum Garden AVM',
+ 		'puan':3,
+ 		'imkanlar':['Dünya Kahveleri','Kekler','Pastalar'],
+ 		'koordinatlar':{
+ 			'enlem':'37.781835',
+ 			'boylam':'30.566034'
+ 		},
+ 		'saatler':[
+ 		  {
+ 			'gunler':'Pazartesi - Cuma ',
+ 			'acilis':' 07:00 ',
+ 			'kapanis':' 23:00 ',
+ 			'kapali':false
+ 		  },
+ 		  {
+ 			'gunler':'Cumartesi ',
+ 			'acilis':' 09:00 ',
+ 			'kapanis':' 22:00 ',
+ 			'kapali':false
+ 			
+ 		  },
+ 		  {
+			'gunler':'Pazar ',
+ 			'kapali':true
+ 		
+ 		  }
+ 		],
+ 		'yorumlar':[
+ 		   {
+ 		   	 'yorumYapan':'Mehmet Armağan TOKLU',
+ 		   	 'puan':1,
+ 		   	 'tarih': '02.12.2020',
+ 		   	 'yorumMetni':'Gitmeyin çay ocağına gidin esnaf kazansın.'
+ 		   },
+ 		   {
+ 		 	 'yorumYapan':'Ahmet Kaan KENAR',
+ 		   	 'puan':5,
+ 		   	 'tarih': '27.11.2020',
+ 		   	 'yorumMetni':'Süper'
+ 		   }
+ 		]
+ 	   }
+ });
 }
+
 const yorumEkle=function(req,res,next){
-  res.render('yorumEkle',{title:'Yorum Yap'});
+ res.render('yorumEkle',{title:'Yorum Ekle'});
 }
 module.exports={
-  anaSayfa,
-  mekanBilgisi,
-  yorumEkle
+anaSayfa,
+mekanBilgisi,
+yorumEkle
 }
