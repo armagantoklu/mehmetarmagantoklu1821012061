eb0ab6834a3afa21ec2027b560d7e05c2ef7b496
mehmetarmagan <armagantoklu1@gmail.com>
2020-12-29 18:39:44 +0300

    ücüncü gonderim

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
+require('./app_api/models/db');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var routesApi = require('./app_api/routes/index');
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
+
+
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
@@ -18,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/api',routesApi);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
